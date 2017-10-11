85441196bab60fe3d60974959403bd05b0a8f176
mezotarazi <mezotarazi@gmail.com>
2017-10-11 14:47:31 -0400

    Controller update

--- app/Http/Controllers/BMIController.php
+++ app/Http/Controllers/BMIController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class BMIController extends Controller
 {
-    //
+    public function index()
+	{
+		return 'Show form to collect data from user'
+	}
+	public function checkform()
+	{
+		return 'Process form'
+		//redirect to results
+	}
 }
